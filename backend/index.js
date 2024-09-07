2f876f90f4d9f6e335383ada9c99742aa10bf9c4
prabhat63s <prabhat6914@gmail.com>
2024-09-07 15:23:52 +0530

    server url change

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://connect-live.onrender.com"],
   credentials: true,
 };
 app.use(cors(corsOption));
